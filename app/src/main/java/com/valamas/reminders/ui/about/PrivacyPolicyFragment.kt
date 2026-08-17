774c83213b02f6acb37ea13713c4f21c796a3053
Mike Vanderkley <valamas@gmail.com>
2026-08-17 22:53:13 +1000

    Store privacy policy locally in app
    - Added privacy_policy.html to res/raw/
    - Updated PrivacyPolicyFragment to load from local file instead of external URL
    - Privacy policy is now bundled with the app, no internet required

--- app/src/main/java/com/valamas/reminders/ui/about/PrivacyPolicyFragment.kt
+++ app/src/main/java/com/valamas/reminders/ui/about/PrivacyPolicyFragment.kt
@@ -25,7 +25,7 @@ class PrivacyPolicyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.webView.loadUrl("https://valamas.github.io/RemindersByValamas/")
+        binding.webView.loadUrl("file:///android_res/raw/privacy_policy.html")
     }
 
     override fun onDestroyView() {
